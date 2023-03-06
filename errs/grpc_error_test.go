d99f3fe08982625a9ff8850c0e2f7c0658079c74
voidint <voidint@126.com>
2023-03-06 20:02:09 +0800

    feat(errs): add function and ut

--- errs/grpc_error_test.go
+++ errs/grpc_error_test.go
@@ -45,3 +45,41 @@ func TestIsGrpcNotFoundError(t *testing.T) {
 		})
 	}
 }
+
+func TestIsGrpcError(t *testing.T) {
+	type args struct {
+		err  error
+		code codes.Code
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "入参是nil",
+			args: args{err: nil, code: codes.Internal},
+			want: false,
+		},
+		{
+			name: "入参是普通error",
+			args: args{err: errors.New("invalid arguments"), code: codes.Internal},
+			want: false,
+		},
+		{
+			name: "入参是grpc中的codes.Internal类型错误",
+			args: args{err: status.Error(codes.Internal, "internal error"), code: codes.Internal},
+			want: true,
+		},
+		{
+			name: "入参是grpc中的codes.NotFound类型错误",
+			args: args{err: status.Error(codes.NotFound, "not found error"), code: codes.Internal},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, IsGrpcError(tt.args.err, tt.args.code))
+		})
+	}
+}
