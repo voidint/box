de38d5967f74fa9a9d2ee9213dd8df3480cc8482
voidint <voidint@126.com>
2023-03-07 15:00:37 +0800

    feat(null): add function and ut

--- null/null.go
+++ null/null.go
@@ -18,3 +18,16 @@ func MustIntFromUint64Ptr(v *uint64) null.Int {
 	}
 	return null.IntFrom(int64(*v))
 }
+
+// MustIntFromUint32Ptr 若入参为nil，则返回无效的null.Int；
+// 若入参为正整数指针，则返回有效的null.Int；
+// 若入参数值大于math.MaxInt64，则发生panic。
+func MustIntFromUint32Ptr(v *uint32) null.Int {
+	if v == nil {
+		return null.NewInt(0, false)
+	}
+	if *v > math.MaxInt32 {
+		panic("parameter value too large")
+	}
+	return null.IntFrom(int64(*v))
+}
--- null/null_test.go
+++ null/null_test.go
@@ -61,3 +61,56 @@ func TestMustIntFromUint64Ptr(t *testing.T) {
 		})
 	}
 }
+
+func TestMustIntFromUint32Ptr(t *testing.T) {
+	var v1 uint32 = 0
+	var v2 uint32 = 7
+	var v3 uint32 = math.MaxInt32
+	var v4 uint32 = math.MaxInt32 + 1
+
+	type args struct {
+		v *uint32
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  null.Int
+		panic bool
+	}{
+		{
+			name: "入参为nil",
+			args: args{v: nil},
+			want: null.NewInt(0, false),
+		},
+		{
+			name: "入参为0",
+			args: args{v: &v1},
+			want: null.NewInt(int64(v1), true),
+		},
+		{
+			name: "入参为正整数",
+			args: args{v: &v2},
+			want: null.NewInt(int64(v2), true),
+		},
+		{
+			name: "入参为math.MaxInt32",
+			args: args{v: &v3},
+			want: null.NewInt(int64(v3), true),
+		},
+		{
+			name:  "入参为极大值（math.MaxInt32+1）",
+			args:  args{v: &v4},
+			panic: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.panic {
+				assert.Panics(t, func() { MustIntFromUint32Ptr(tt.args.v) })
+
+			} else {
+				assert.Equal(t, tt.want, MustIntFromUint32Ptr(tt.args.v))
+			}
+		})
+	}
+}
