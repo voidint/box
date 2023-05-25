e98cede3ce059946fd17eff900cf9dd09758aaf3
voidint <voidint@126.com>
2023-05-25 18:00:05 +0800

    feat(null): add some functions about time

--- null/null.go
+++ null/null.go
@@ -2,6 +2,7 @@ package null
 
 import (
 	"math"
+	"time"
 
 	"gopkg.in/guregu/null.v4"
 )
@@ -30,4 +31,28 @@ func MustIntFromUint32Ptr(v *uint32) null.Int {
 		panic("parameter value too large")
 	}
 	return null.IntFrom(int64(*v))
+}
+
+// TimeFromUnixInt64Ptr 若入参为nil，则返回无效的null.Time；若入参非nil，则返回指定秒（Unix时间戳）的时间。
+func TimeFromUnixInt64Ptr(v *int64) null.Time {
+	if v == nil {
+		return null.NewTime(time.Time{}, false)
+	}
+	return null.TimeFrom(time.Unix(*v, 0))
+}
+
+// TimeFromUnixMilliInt64Ptr 若入参为nil，则返回无效的null.Time；若入参非nil，则返回指定毫秒（Unix时间戳）的时间。
+func TimeFromUnixMilliInt64Ptr(v *int64) null.Time {
+	if v == nil {
+		return null.NewTime(time.Time{}, false)
+	}
+	return null.TimeFrom(time.UnixMilli(*v))
+}
+
+// TimeFromUnixMicroInt64Ptr 若入参为nil，则返回无效的null.Time；若入参非nil，则返回指定微秒（Unix时间戳）的时间。
+func TimeFromUnixMicroInt64Ptr(v *int64) null.Time {
+	if v == nil {
+		return null.NewTime(time.Time{}, false)
+	}
+	return null.TimeFrom(time.UnixMicro(*v))
 }
